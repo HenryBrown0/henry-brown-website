5f6233d1d650db2addc57930c2a4139ed0d5d3fa
HenryBrown0 <henrybrown444@gmail.com>
2019-08-04 13:09:37 +0100

    Add process en port to server

--- server.ts
+++ server.ts
@@ -1,4 +1,5 @@
 import express = require('express');
+const PORT = process.env.PORT || 3000
 
 const app: express.Application = express();
 
@@ -6,6 +7,6 @@ app.get('/', ((request, response) => {
 	response.send('Hello world')
 }));
 
-app.listen(3000, (() => {
-	console.info('\x1b[33m%s\x1b[0m', 'Listening on port 3000');
+app.listen(PORT, (() => {
+	console.info('\x1b[33m%s\x1b[0m', `Listening on port ${PORT}`);
 }));
