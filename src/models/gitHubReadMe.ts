18bb90decddc7fc11276a80967145a62e910211c
HenryBrown0 <me@henrybrown0.com>
2020-03-27 10:23:51 +0000

    Handle project erros better

--- src/models/gitHubReadMe.ts
+++ src/models/gitHubReadMe.ts
@@ -3,7 +3,8 @@ import renderHTML from '../helpers/markdownToHTML';
 
 const getReadMe = async (name: string): Promise<string> => {
 	if (!name) {
-		throw new Error('No repository name provided');
+		console.warn('No repository name provided');
+		return null;
 	}
 
 	const uri = `https://raw.githubusercontent.com/HenryBrown0/${name}/master/README.md`;
@@ -12,12 +13,14 @@ const getReadMe = async (name: string): Promise<string> => {
 		https.get(uri, (response) => {
 			if (response.statusCode !== 200) {
 				response.resume();
-				reject('Non 200 status code from read me');
+				console.warn('Non 200 status code from read me');
+				return resolve(null);
 			}
 
 			if (!/^text\/plain/.test(response.headers['content-type'])) {
 				response.resume();
-				reject('Incorrect file formate from read me');
+				console.warn('Incorrect file formate from read me');
+				return resolve(null);
 			}
 
 			response.setEncoding('utf8');
@@ -26,7 +29,7 @@ const getReadMe = async (name: string): Promise<string> => {
 			response.on('data', (chunk) => rawData += chunk);
 			response.on('end', () => resolve(renderHTML(rawData)));
 		}).on('error', (error) => {
-			reject(error);
+			return reject(error);
 		});
 	});
 };
