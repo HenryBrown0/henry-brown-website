712bea5db97ad0c63b85055d563511bb5b230c04
HenryBrown0 <me@henrybrown0.com>
2020-04-21 14:27:24 +0100

    Add cloudflare whitelist

--- src/server.ts
+++ src/server.ts
@@ -5,21 +5,30 @@ import * as sentry from '@sentry/node';
 import routes from './routes';
 import setupPartials from './setupPartials';
 import { captureException } from './helpers/logger';
+import cloudflareProxy from './lib/cloudflareProxy';
 
-const { SENTRY_DSN, NODE_ENV, PORT } = process.env;
+const { SENTRY_DSN, NODE_ENV = 'development', PORT } = process.env;
 
 const app = express();
 
 sentry.init({
 	dsn: SENTRY_DSN,
-	environment: NODE_ENV || 'development',
+	environment: NODE_ENV,
 });
+
+if (NODE_ENV === 'production' || NODE_ENV === 'staging') {
+	app.set('trust proxy', true);
+}
 
 app.set('view engine', 'squirrelly');
 app.set('views', 'views');
 
 app.use(sentry.Handlers.requestHandler());
 app.use(helmet());
+
+if (NODE_ENV === 'production' || NODE_ENV === 'staging') {
+	app.use(cloudflareProxy);
+}
 
 app.use('/', routes);
 app.use('/static', express.static(path.join(__dirname, 'static'), {
