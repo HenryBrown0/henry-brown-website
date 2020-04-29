3571a40ce93f2c1cf9cc533b891b4c6c0f8898ec
HenryBrown0 <me@henrybrown0.com>
2020-04-29 21:44:50 +0100

    Add new repository table

--- schema.sql
+++ schema.sql
@@ -19,3 +19,15 @@ CREATE TABLE IF NOT EXISTS service (
 	isBackgroundDark BOOLEAN NOT NULL,
 	content TEXT NOT NULL
 );
+
+CREATE TABLE IF NOT EXISTS repository (
+	id TEXT PRIMARY KEY,
+	name TEXT NOT NULL,
+	description TEXT,
+	updated_at TIMESTAMP NOT NULL,
+	is_archived BOOLEAN NOT NULL,
+	background_color CHAR(6) NOT NULL,
+	is_background_dark BOOLEAN NOT NULL,
+	git_hub_url TEXT NOT NULL,
+	read_me TEXT
+);
