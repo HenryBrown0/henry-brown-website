bb14f9ee4c0571e1943b954f120129aaefa68ceb
HenryBrown0 <me@henrybrown0.com>
2020-04-29 21:45:39 +0100

    Add repository and gitHub models

---
+++ src/models/gitHub.ts
@@ -0,0 +1,144 @@
+import { Severity } from '@sentry/node';
+import tinyColor from 'tinycolor2';
+import { captureMessage, captureException } from '../helpers/logger';
+import getGraphQLClient from '../helpers/github';
+import transformUriToName from '../helpers/transformUriToName';
+
+const GIT_HUB_LOGIN = 'HenryBrown0';
+const REPOSITORY_LIMIT = 20;
+
+interface IRawGitHubRepository {
+	name: string,
+	description: string;
+	updatedAt: string;
+	isArchived: boolean,
+	primaryLanguage: {
+		color: string;
+	};
+	url: string;
+}
+
+const transformGitHubRepository = (repository: IRawGitHubRepository): IRepository => {
+	const backgroundColor = repository.primaryLanguage.color;
+	return {
+		id: repository.name,
+		name: transformUriToName(repository.name),
+		description: repository.description,
+		updatedAt: new Date(repository.updatedAt),
+		isArchived: repository.isArchived,
+		backgroundColor: backgroundColor.split('#')[1],
+		isBackgroundDark: tinyColor(backgroundColor).isDark(),
+		gitHubUrl: repository.url,
+	};
+};
+
+const gitHubRepositoriesQuery = `
+	query getRepositories($login: String!, $limit: Int!) {
+		user(login: $login) {
+			repositories(
+				first: $limit
+				ownerAffiliations: OWNER
+				privacy: PUBLIC
+				orderBy: {
+					field: UPDATED_AT
+					direction: DESC
+				}
+			) {
+				edges {
+					node {
+						name
+						description
+						updatedAt
+						isArchived
+						primaryLanguage {
+							color
+						}
+						url
+					}
+				}
+			}
+		}
+	}
+`;
+
+const gitHubRepositoryQuery = `
+	query getRepository($login: String!, $name: String!) {
+		user(login: $login) {
+			repository(name: $name) {
+				name
+				description
+				updatedAt
+				isArchived
+				primaryLanguage {
+					color
+				}
+				url
+				isPrivate
+			}
+		}
+	}
+`;
+
+const create = (): void => {
+	captureMessage('Not yet implemented', Severity.Debug);
+};
+
+const read = async (name?: string): Promise<IRepository[]> => {
+	const graphQLClient = getGraphQLClient();
+
+	if (name) {
+		try {
+			const gitHubRepository = await graphQLClient.request(
+				gitHubRepositoryQuery,
+				{
+					login: GIT_HUB_LOGIN,
+					name,
+				},
+			);
+
+			if (!gitHubRepository
+				|| !gitHubRepository.user.repository
+				|| gitHubRepository.user.repository.isPrivate
+			) {
+				return [];
+			}
+
+			return [transformGitHubRepository(gitHubRepository.user.repository)];
+		} catch (error) {
+			captureException(error);
+			return [];
+		}
+	}
+
+	try {
+		const gitHubRepositories = await graphQLClient.request(
+			gitHubRepositoriesQuery,
+			{
+				login: GIT_HUB_LOGIN,
+				limit: REPOSITORY_LIMIT,
+			},
+		);
+		if (!gitHubRepositories) return [];
+		return gitHubRepositories.user.repositories.edges.map((rawRepositoryEdge: any) => (
+			transformGitHubRepository(rawRepositoryEdge.node)
+		));
+	} catch (error) {
+		captureException(error);
+		return [];
+	}
+};
+
+const update = (): void => {
+	captureMessage('Not yet implemented', Severity.Debug);
+};
+
+const remove = (): void => {
+	captureMessage('Not yet implemented', Severity.Debug);
+};
+
+export default {
+	create,
+	read,
+	update,
+	remove,
+};
---
+++ src/models/repository.ts
@@ -0,0 +1,120 @@
+import databaseQuery from '../lib/database';
+import { captureException } from '../helpers/logger';
+
+/* eslint-disable camelcase */
+interface IRawRepository {
+	id: string;
+	name: string;
+	description: string;
+	updated_at: Date;
+	is_archived: boolean;
+	background_color: string;
+	is_background_dark: boolean;
+	git_hub_url: string;
+	read_me: string;
+}
+/* eslint-enable camelcase */
+
+const transformRepository = (repository: IRawRepository): IRepository => (
+	{
+		id: repository.id,
+		name: repository.name,
+		description: repository.description,
+		updatedAt: new Date(repository.updated_at),
+		isArchived: repository.is_archived,
+		backgroundColor: repository.background_color,
+		isBackgroundDark: repository.is_background_dark,
+		gitHubUrl: repository.git_hub_url,
+		readMe: repository.read_me,
+	}
+);
+
+const create = async (repository: IRepository): Promise<Boolean> => {
+	try {
+		await databaseQuery(
+			'INSERT INTO repository (id, name, description, updated_at, is_archived, background_color, is_background_dark, git_hub_url, read_me) VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9)',
+			[
+				repository.id,
+				repository.name,
+				repository.description,
+				repository.updatedAt,
+				repository.isArchived,
+				repository.backgroundColor,
+				repository.isBackgroundDark,
+				repository.gitHubUrl,
+				repository.readMe,
+			],
+		);
+		return true;
+	} catch (error) {
+		captureException(error);
+		return false;
+	}
+};
+
+const read = async (name?: string): Promise<IRepository[]> => {
+	if (name) {
+		try {
+			const queryResult = await databaseQuery(
+				'SELECT id, name, description, updated_at, is_archived, background_color, is_background_dark, git_hub_url, read_me FROM repository WHERE id=$1',
+				[name],
+			);
+			if (!queryResult.rowCount) return [];
+			return queryResult.rows.map(transformRepository);
+		} catch (error) {
+			captureException(error);
+			return [];
+		}
+	}
+
+	try {
+		const queryResult = await databaseQuery(
+			'SELECT id, name, description, updated_at, is_archived, background_color, is_background_dark, git_hub_url, read_me FROM repository',
+		);
+		if (!queryResult.rowCount) return [];
+		return queryResult.rows.map(transformRepository);
+	} catch (error) {
+		captureException(error);
+		return [];
+	}
+};
+
+const update = async (repository: IRepository): Promise<boolean> => {
+	try {
+		await databaseQuery(
+			'UPDATE repository SET name=$2, description=$3, updated_at=$4, is_archived=$5, background_color=$6, is_background_dark=$7, git_hub_url=$8, read_me=$9 WHERE id=$1',
+			[
+				repository.id,
+				repository.name,
+				repository.description,
+				repository.updatedAt,
+				repository.isArchived,
+				repository.backgroundColor,
+				repository.isBackgroundDark,
+				repository.gitHubUrl,
+				repository.readMe,
+			],
+		);
+		return true;
+	} catch (error) {
+		captureException(error);
+		return false;
+	}
+};
+
+const remove = async (id: string): Promise<boolean> => {
+	try {
+		await databaseQuery('DELETE FROM repository WHERE id=$1', [id]);
+		return true;
+	} catch (error) {
+		captureException(error);
+		return false;
+	}
+};
+
+export default {
+	create,
+	read,
+	update,
+	remove,
+};
